4e7cbc338df0cb5a4a4149cf814fefbe446f3e9b
EmilyBarausse <emilybarausse2003@gmail.com>
2024-03-26 14:43:45 -0300

    lib finalizada

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,8 +1,14 @@
+import chalk from "chalk";
+
 async function checaStatus(listaURLs){
     const arrStatus= await Promise.all(
             listaURLs.map(async(url)=>{
-            const response = await fetch (url);
-            return response.status;
+                try{
+                    const response = await fetch (url);
+                    return response.status;
+                }catch(erro){
+                    return manejaErros(erro);
+                }
         })
     )
     return arrStatus;
@@ -12,13 +18,25 @@ function extraiLinks(arrLinks){
     return arrLinks.map((objetoLink)=>Object.values(objetoLink).join());
 }
 
+function manejaErros(erro){
+    //console.log(chalk.red("algo deu errado"))
+    if(erro.cause.code==="ENOTFOUND"){
+        return "Link não encontrado"
+    }else{
+        return "Ocorreu um erro"
+    }
+}
+
 //criar uma função listaValidada
 export default async function listaValidada(listaDeLinks){
     // return extraiLinks(listaDeLinks);
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
     // console.log(status);
-    return status;
+    return listaDeLinks.map((objeto,indice)=>({
+        ...objeto,
+        status:status[indice]
+    }));
 }
 
 //[gatinho salsicha](http://gatinhosalsicha.com.br/)
